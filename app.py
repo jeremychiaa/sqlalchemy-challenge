26f96979e672ae39a26241777c918b43bac04d1b
Jeremy Chia <jeremychiajianzhi@gmail.com>
2020-10-28 01:04:38 +1100

    app flask routes

--- app.py
+++ app.py
@@ -31,3 +31,29 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate App API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation<br/>")
+
+@app.route("/api/v1.0/stations<br/>")
+
+@app.route("/api/v1.0/tobs<br/>")
+
+@app.route("/api/v1.0/<start><br/>")
+
+@app.route("/api/v1.0/<start>/<end><br/>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
