d54c5a545cc69b831c7f83235112ddc10f0dc084
Jeremy Chia <jeremychiajianzhi@gmail.com>
2020-10-28 23:43:52 +1100

    climate app stations

--- app.py
+++ app.py
@@ -45,15 +45,60 @@ def welcome():
     )
 
 
-@app.route("/api/v1.0/precipitation<br/>")
+@app.route("/api/v1.0/precipitation")
+def precipitation():
 
-@app.route("/api/v1.0/stations<br/>")
+    # Create session from Python to the database
+    session = Session(engine)
 
-@app.route("/api/v1.0/tobs<br/>")
+    # Query all precipition data
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
-@app.route("/api/v1.0/<start><br/>")
+    session.close()
 
-@app.route("/api/v1.0/<start>/<end><br/>")
+    # Create a dictionary from the row data and append to a list of all precipitation
+    all_precipitation = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        all_precipitation.append(precipitation_dict)
+
+    # Return JSON representation of dictionary
+    return jsonify(all_precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    # Create session from Python to the database
+    session = Session(engine)
+    
+    # Query all stations
+    results = session.query(Station.station, Station.name,
+                            Station.latitude, Station.longitude, Station.elevation).all()
+
+    session.close()
+    
+    # Create a dictionary from the row data and append to a list of all stations
+    all_stations = []
+    for station, name, latitude, longitude, elevation in results:
+        stations_dict = {}
+        stations_dict["station"] = station
+        stations_dict["name"] = name
+        stations_dict["latitude"] = latitude
+        stations_dict["longitude"] = longitude
+        stations_dict["elevation"] = elevation
+        all_stations.append(stations_dict)
+
+    # Return JSON representation of dictionary
+    return jsonify(all_stations)
+
+
+#@app.route("/api/v1.0/tobs")
+
+#@app.route("/api/v1.0/<start>")
+
+#@app.route("/api/v1.0/<start>/<end>")
 
 if __name__ == "__main__":
     app.run(debug=True)
